857f2ab28bff12067aee2d137f53f3633b712467
Andrew Weir <andru.weir@gmail.com>
2016-07-21 16:51:04 +0700

    Added falsy test for dispatcher

--- test/unit/dispatcher.js
+++ test/unit/dispatcher.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const sinon  = require('sinon');
 const should = require('should');
 require('should-sinon');
@@ -6,21 +8,29 @@ const path = require('path');
 const jobsPath = path.join(path.dirname(__filename), '..', '..', 'examples', 'jobs');
 const Dispatcher = require('../../lib/dispatcher');
 
-/**
- * Mock Queue
- */
-const queue = {
-  create:   sinon.stub().returnsThis(),
-  priority: sinon.stub().returnsThis(),
-  save:     sinon.stub().returnsThis(),
-};
-
-/**
- * Publisher Mock
- */
-const publisher = { publish: sinon.stub() };
+let queue;
+let publisher;
 
 describe('Dispatcher', function () {
+
+  beforeEach(function (done) {
+    /**
+     * Mock Queue
+     */
+    queue = {
+      create:   sinon.stub().returnsThis(),
+      priority: sinon.stub().returnsThis(),
+      save:     sinon.stub().returnsThis(),
+    };
+
+    /**
+     * Publisher Mock
+     */
+    publisher = { publish: sinon.stub() };
+
+    done();
+  });
+
   describe('dispatch', function () {
     it('should dispatch a job to the queue', function (done) {
       const dispatcher = new Dispatcher('q', queue, publisher, jobsPath);
@@ -30,7 +40,20 @@ describe('Dispatcher', function () {
       queue.priority.should.be.calledOnce().calledWith(0);
       queue.save.should.be.calledOnce();
 
-      publisher.publish.should.be.calledOnce('q', 'email');
+      publisher.publish.should.be.calledOnce().calledWith('q', 'email');
+
+      done();
+    });
+
+    it('should not dispatch if job file not found', function (done) {
+      const dispatcher = new Dispatcher('q', queue, publisher, jobsPath);
+      dispatcher.dispatch('do-not-exist', 'andrew@ekoapp.com');
+
+      queue.create.should.not.be.calledOnce();
+      queue.priority.should.not.be.calledOnce();
+      queue.save.should.not.be.calledOnce();
+
+      publisher.publish.should.not.be.calledOnce();
 
       done();
     });
